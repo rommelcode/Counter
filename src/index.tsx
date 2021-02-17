c264476a629fe0263491b0ca96e290a6289b3645
rommelcode <68412545+rommelcode@users.noreply.github.com>
2021-02-17 11:33:15 +1100

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import Person from './Person';
+import Counter from './Counter';
 
 import './styles.css';
 
 function App(): JSX.Element {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
 }
 
 const rootElement = document.getElementById('root');
